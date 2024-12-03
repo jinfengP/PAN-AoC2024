585a6cddc4984fae0bebcc5501edd68e1d764e94
PanMan <146111571+jinfengP@users.noreply.github.com>
2024-12-02 19:11:27 -0500

    Update 2024Day1.py

--- 2024Day1.py
+++ 2024Day1.py
@@ -6,7 +6,6 @@ def get_file_data(file_name):
     return data
 list = get_file_data("input.txt")
 
-
 list1 = [] #first half of pair
 list2 = [] #the other pair
 for i in range(len(list)):
@@ -17,13 +16,25 @@ for i in range(len(list)):
 # splits it into two lists each with half the pair
 list1.sort()
 list2.sort()
-sum = 0
 
-for i in range(len(list)):
-    num1 = list1[i]
-    count = 0
-    for j in range(len(list)):
-        if num1 == list2[j]:
-            count+=1
-    sum += int(num1) * count
-print(sum)
+def totalDistance(): # PART ONE---------------------
+    sum = 0
+    for i in range(len(list)):
+        diff = int(list1[i]) - int(list2[i])
+        sum += abs(diff)
+    return sum
+
+
+def calculateSimilarity(): # PART TWO---------------
+    sum = 0
+    for i in range(len(list)):
+        num1 = list1[i]
+        count = 0
+        for j in range(len(list)):
+            if num1 == list2[j]:
+                count+=1
+        sum += int(num1) * count
+    return sum
+
+print(totalDistance())
+print(calculateSimilarity())

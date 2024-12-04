2bc410d5589644db5e88886d2508ee44573dc8de
PanMan <146111571+jinfengP@users.noreply.github.com>
2024-12-03 19:33:40 -0500

    Update 2024Day3.py

--- 2024Day3.py
+++ 2024Day3.py
@@ -7,21 +7,40 @@ def get_file_data(file_name):
         data += (line.rstrip())
     return data # a string of LITERALLY EVERYTHING.
 
-
 searchString = get_file_data("input.txt")
 
-
-def partOne():
+def filterAndMultiply():
     sumOfProducts = 0
-    firstNumList = []
-    secondNumList = []
-    regex = "mul\\([1-9]{1,3},[1-9]{1,3}\\)"
+    numList = []
+    regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)"
     filteredData = re.findall(regex,searchString)
-    print(filteredData)
-    for element in filteredData:
-        print(element)
-        firstNumSearch = "\\([1-9]{1,3}"
-        firstNumList = re.findall(firstNumSearch,element)
-        print(firstNumList)
+    for i in range(len(filteredData)):
+        firstNumSearch = "[0-9]{1,3}"
+        numList.append(re.findall(firstNumSearch,filteredData[i]))
+    for index in range(len(numList)):
+        sumOfProducts += int(numList[index][0]) * int(numList[index][1])
+    #     print("Current Sum: " + str(sumOfProducts) + " by adding the product of " + str(numList[index][0]) + " and " + str(numList[index][1]))
+    # print("Sum: " + str(sumOfProducts))
+    # print("Filtered Data: " + str(filteredData))
     return sumOfProducts
-partOne()
+
+def partTwo():
+    sumOfProducts = 0
+    consider = True
+    numList = []
+    regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)"
+    filteredData = re.findall(regex,searchString) # GOOD
+    for i in range(len(filteredData)):
+        firstNumSearch = "[0-9]{1,3}"
+        numList.append(re.findall(firstNumSearch,filteredData[i])) #includes length 0 arrays which are either do() or don't()
+    for i in range(len(numList)):
+        if filteredData[i] == "do()":
+            consider = True
+        elif filteredData[i] == "don't()":
+            consider = False
+        else:
+            if consider:
+                sumOfProducts += int(numList[i][0]) * int(numList[i][1])
+    return sumOfProducts
+
+

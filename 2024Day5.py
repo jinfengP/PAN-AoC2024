a22d1b996e644bc52f7972427da7c7d6bf2697d8
PanMan <146111571+jinfengP@users.noreply.github.com>
2024-12-05 21:43:33 -0500

    Update 2024Day5.py

--- 2024Day5.py
+++ 2024Day5.py
@@ -1,4 +1,3 @@
-
 def get_rules(file_name):
     f = open(file_name)
     data = []
@@ -18,29 +17,32 @@ def get_updates(file_name):
 rules = get_rules("input.txt")
 updates = get_updates("input.txt")
 
-
-
 def checkUpdateOrderThenAddMiddleNumber():
     sumOfMiddleNums = 0
-    # rules - for each update, look for the first number. take its index.
+    # for each update, look for the first number. take its index.
     # then look for the second number. take its index. if index 2 < index 1, invalid.
     for u in range(len(updates)):
+        ruleBroken = False
+        sumOfCurrentRow = 0
         for r in range(len(rules)):
             firstNumIndex = -1
             secondNumIndex = -1
             # each number is always 2 digits long
-            firstNum = rules[r][0:1]
-            secondNum = rules[r][3:4]
+            firstNum = rules[r][0:2]
+            secondNum = rules[r][3:5]
             for num in range(len(updates[u])):
-                if firstNum in updates[u]: #if found, look through updates[u] to see where
-                    firstNumIndex = u
-                if secondNum in updates[u]:
-                    secondNumIndex = u
-            if not (firstNumIndex == -1) and not (secondNumIndex == -1):
-                if int(firstNumIndex) < int(secondNumIndex): #this makes it valid!
-                    sumOfMiddleNums += int(updates[u][len(updates[u])//2])
-                    print("weewoo" + str(int(updates[u][len(updates[u])//2])))
+                if firstNum in updates[u][num]: #if found, look through updates[u] to see where
+                    firstNumIndex = num
+                if secondNum in updates[u][num]:
+                    secondNumIndex = num
+            if not (firstNumIndex == -1) and not (secondNumIndex == -1): #if both are found then
+                print("ttt " + str(firstNumIndex) + " " + str(secondNumIndex)) 
+                if (not ruleBroken) and (int(firstNumIndex) < int(secondNumIndex)): #this makes it valid!
+                    sumOfCurrentRow = int(updates[u][len(updates[u])//2])
+                    print("weewoo " + str(int(updates[u][len(updates[u])//2])))
+                elif (int(firstNumIndex) > int(secondNumIndex)):
+                    print("Rule has been broken " + str(rules[r]))
+                    ruleBroken = True
+                    sumOfCurrentRow = 0
+        sumOfMiddleNums += sumOfCurrentRow
     return sumOfMiddleNums
-
-print(checkUpdateOrderThenAddMiddleNumber())
-
